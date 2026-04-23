d3544ed72dd1d42ed814502ff05790d6d2ba5341
Yashec21 <yashpatilec21@gmail.com>
2026-04-23 09:55:13 +0530

    Add files via upload

---
+++ Access.java
@@ -0,0 +1,35 @@
+class Access
+{
+    public static void main(String A[])
+
+    {
+        Demo dobj = new Demo();
+
+        dobj.Display();
+
+        System.out.println("value of i :"+dobj.i); //Allowed
+        System.out.println("value of j :"+dobj.j); //Not allowed
+        System.out.println("value of k :"+dobj.k); //????
+    }
+}
+
+class Demo
+{
+    public int i ;
+    private int j ;
+    protected int k ;
+
+    public Demo()
+    {
+        i = 0 ;
+        j = 0 ;
+        k = 0 ;
+    }
+
+    public void Display()
+    {
+        System.out.println("value of i :"+i); //Allowed
+        System.out.println("value of j :"+j); //Allowed
+        System.out.println("value of k :"+k); //Allowed
+    }
+}
